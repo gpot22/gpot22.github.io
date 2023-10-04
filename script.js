7fc20e9bf17c606c887cf7c3c649509635f8135f
caden <mykkchan@gmail.com>
2023-10-04 05:03:01 -0400

    added portfolio page + items
    lots of messy code to brush over

--- script.js
+++ script.js
@@ -20,3 +20,55 @@ window.addEventListener('load', () => {
     let rect = navItemRects[idx].getBoundingClientRect()
     setNavIndicator(rect)
 })
+
+let counters = []
+
+const projects = document.querySelectorAll(".folio-project")
+console.log(projects)
+projects.forEach((project, index) => {
+    prepSlides(project, index)
+})
+
+function prepSlides(project, index) {
+    const slides = project.querySelectorAll(".slide")
+    slides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`;
+    });
+
+    const nextBtn = project.querySelector(".next-btn");
+    const prevBtn = project.querySelector(".prev-btn");
+
+    counters.push(0)
+    nextBtn.addEventListener("click", function () {
+    counters[index]++;
+    carousel(slides, counters[index], nextBtn, prevBtn);
+    });
+
+    prevBtn.addEventListener("click", function () {
+    counters[index]--;
+    carousel(slides, counters[index], nextBtn, prevBtn);
+    });
+    prevBtn.style.opacity = 0;
+    prevBtn.style.pointerEvents = "none"
+}
+
+function carousel(slides, counter, nextBtn, prevBtn) {
+
+  if (counter < slides.length - 1) {
+    nextBtn.style.opacity = 1;
+    nextBtn.style.pointerEvents = "auto"
+  } else {
+    nextBtn.style.opacity = 0;
+    nextBtn.style.pointerEvents = "none"
+  }
+  if (counter > 0) {
+    prevBtn.style.opacity = 1;
+    prevBtn.style.pointerEvents = "auto"
+  } else {
+    prevBtn.style.opacity = 0;
+    prevBtn.style.pointerEvents = "none"
+  }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
